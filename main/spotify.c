5a9ab82765b53eaf6998ce209a7d7fd3faeb9c35
Michal Ciesielski <michal.ciesielski@cybercom.com>
2020-11-14 16:31:51 +0100

    cJSON_Delete which fixes big memory leak

--- main/spotify.c
+++ main/spotify.c
@@ -282,6 +282,7 @@ static esp_err_t spotify_http_event_handler(esp_http_client_event_t *evt)
           }
         }
 
+        cJSON_Delete(response_json);
       }
       ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
       break;
