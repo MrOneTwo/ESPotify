44afe7b005ea9b21a209f1065af179961b63caf0
Michal Ciesielski <ciesielskimm@gmail.com>
2020-11-16 16:18:28 +0100

    Comments

--- main/spotify.h
+++ main/spotify.h
@@ -22,16 +22,34 @@ typedef struct spotify_playback_t
   char song_id[32];
 } spotify_playback_t;
 
+// TODO(michalc): instead of making it extern and passing it every time it's probably better to
+// just operate on it inside the spotify module and never expose it.
 extern spotify_access_t spotify;
 extern spotify_playback_t spotify_playback;
 
+/*
+ * Init structures - mostly about setting the access tokens/codes.
+ */
 void spotify_init(spotify_access_t* spotify);
-void spotify_query(spotify_access_t* spotify);
+
+/*
+ * Use the refresh token to update the access token. Access token expires after 1 hour.
+ */
 void spotify_refresh_access_token(spotify_access_t* spotify);
 
-void spotify_enqueue_song(spotify_access_t* spotify, const char* const song_id);
-// https://api.spotify.com/v1/me/player/queue?uri=spotify:track:ID
+/*
+ * This updates the spotify_playback_t static structure.
+ */
+void spotify_query(spotify_access_t* spotify);
 
+/*
+ * Push a song to the Spotify's queue.
+ */
+void spotify_enqueue_song(spotify_access_t* spotify, const char* const song_id);
+
+/*
+ * Make Spotify jump to the next song in the queue.
+ */
 void spotify_next_song(spotify_access_t* spotify);
 
 #endif // SPOTIFY_H
